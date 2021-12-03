d40028e0d850b2675f07b54d1f564756a0a164ab
timmoll <91943978+timmoll@users.noreply.github.com>
2021-12-03 13:42:33 +0100

    pagina 1 af

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
 // JavaScript Document
+
+var uitgeklapt = document.querySelector(".dpmenu");
+var ingeklapt = document.querySelector(".kruis");
+var menu = document.querySelector(".menu")
+
+uitgeklapt.addEventListener('click', function (){
+  menu.classList.add ("openmenu")
+  console.log('hoi')
+})
+
+ingeklapt.addEventListener('click', function (){
+  menu.classList.remove ("openmenu")
+})
+
